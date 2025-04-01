b342684e8bb3b3d7c96cd66304bb5cbce4371b49
CarlosBF <cborrellf@outlook.es>
2025-04-01 20:57:28 +0200

    Improved version after live review

--- js/script.js
+++ js/script.js
@@ -4,11 +4,14 @@ const result = document.getElementById('result')
 const restart = document.getElementById('restart')
 let seconds = 5
 
-all()
+restart.addEventListener('click', start)
 
-restart.addEventListener('click', all)
+start()
 
-function all() {
+
+/* FIRST SOLUTION
+
+function start() {
     result.innerHTML=''
     const myInterval = setInterval(reduceCountdown, 1000)
     countdownFunction().then((resolve) =>{ 
@@ -28,7 +31,6 @@ function all() {
     })
 }
 
-
 function countdownFunction(){
     return new Promise((resolve) => {
         setTimeout(() => {
@@ -36,9 +38,39 @@ function countdownFunction(){
             resolve(ans) 
         }, 6100)
     })
+}*/
+
+/* LIVE REVIEW SOLUTION */
+function start() {
+    restart.style.display = 'none'
+    result.innerHTML=''
+    seconds = 5
+    const intervalo = setInterval(() => {
+        if(seconds === 0){
+            clearInterval(intervalo)
+            let ans = 1 + Math.floor(Math.random()*3)
+            checkResult(ans)
+            restart.style.display = ''
+        }
+        reduceCountdown()
+    }, 1000);
 }
 
 function reduceCountdown() {
     countdown.textContent = 'Cuenta atrás: '+seconds+' segundos'
     seconds--
+}
+
+function checkResult(ans) {
+    if(ans == userInput.value){
+        result.innerHTML = `
+        <p class = "green">Enhorabuena, has salvado el mundo</p>
+        <p>Tu número ${userInput.value} es el mismo que el número ${ans}</p>
+        `
+    }else{
+        result.innerHTML = `
+        <p class = "red">Fallaste, la bomba ha estallado</p>
+        <p>Tu número ${userInput.value} es diferente al número ${ans}</p>
+        `
+    }
 }
